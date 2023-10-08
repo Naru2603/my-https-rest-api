d692616bbd5f5cdd5ec693aa0a4d6030cb384585
Naru2603 <naruck1997@gmail.com>
2023-10-08 11:47:52 +0530

    https rest api

---
+++ src/main/java/com/infy/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.infy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String Hello() {
+		return "Hello World..!!!";
+		
+	}
+}
